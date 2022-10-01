55a6461a3d8cf1b61aed9099b057a2a800984789
thguss <thgus345@naver.com>
2022-10-01 15:38:47 +0900

    [semiar1] 배열

---
+++ seminar1/array.ts
@@ -0,0 +1,16 @@
+let numbers: number[] = [1, 2, 3];
+
+const strings: Array<String> = ['hi', 'hello'];
+
+// 대문자가 좀 더 엄격
+const objArray1: Object[] = [
+  { item1: 'oh' },
+  { item2: 'wow' }
+];
+console.log(objArray1);
+
+const objArray2: object[] = [
+  { item1: 'oh' },
+  { item2: 'wow' }
+];
+console.log(objArray2);
