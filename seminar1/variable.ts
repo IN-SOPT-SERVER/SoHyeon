76625ed5803905228f2c40e9b10b097c0da02440
thguss <thgus345@naver.com>
2022-10-01 15:35:17 +0900

    [seminar1] ts 변수

---
+++ seminar1/variable.ts
@@ -0,0 +1,10 @@
+const isLiked: boolean = true;
+console.log(`${typeof isLiked}, ${isLiked}`);
+
+const str: string = 'hello';
+console.log(`${typeof str}, ${str}`);
+
+let num: number = 31;
+console.log(`${typeof num}, ${num}`);
+
+// const sum: number = `sum number`; // variable.ts:10:7 - error TS2322: Type 'string' is not assignable to type 'number'.
