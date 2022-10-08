f786dc568b4eed600dd585c762467a8e735846c1
thguss <thgus345@naver.com>
2022-10-08 16:46:59 +0900

    [week2] 기본 과제

---
+++ seminar2-express/src/api/user.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response, Router } from "express";
+// express 모듈에서 express, (request, response, router)-> 타입 정의를 위해 불러옴!
+
+const router: Router = express.Router();
+
+router.get("/", (req: Request, res: Response) => {
+
+  const user = "하이 ㅋㅋ";
+
+  return res.status(200).json({
+    status: 200,
+    message: "유저 조회 성공",
+    data: user,
+  });
+});
+
+module.exports = router;
