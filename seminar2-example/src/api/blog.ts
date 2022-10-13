e3b81c8348b94940ab4486d47eb78d2ce988e3ab
thguss <thgus345@naver.com>
2022-10-13 23:25:14 +0900

    [week2] DELET: 미사용 import 코드 삭제

--- seminar2-example/src/api/blog.ts
+++ seminar2-example/src/api/blog.ts
@@ -1,6 +1,4 @@
 import express, { Request, Response, Router } from "express";
-import infos from "./data/members";
-// express 모듈에서 express, (request, response, router)-> 타입 정의를 위해 불러옴!
 
 const router: Router = express.Router();
 
